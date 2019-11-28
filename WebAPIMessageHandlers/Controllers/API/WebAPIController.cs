12d50ed3b310f1141a15e8da600fcddcb74dd3dc
GowthamanJegan <gowthambe91@gmail.com>
2019-11-28 18:11:50 +0530

    WebAPIMessageHandlers - initial commit

---
+++ WebAPIMessageHandlers/Controllers/API/WebAPIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace WebAPIMessageHandlers.Controllers.API
+{
+    public class WebAPIController : ApiController
+    {
+        public IHttpActionResult GetApiProcessLifeCycle()
+        {
+            //return Ok(File.ReadAllBytes(@"D:\GitHUB\WebAPIMessageHandlers\WebAPIMessageHandlers\Content\Images\aspnet-web-api-poster.pdf"));//Returns the bytes as string
+
+            //byte[] bytes = File.ReadAllBytes(@"D:\GitHUB\WebAPIMessageHandlers\WebAPIMessageHandlers\Content\Images\aspnet-web-api-poster.pdf");
+            //HttpRequestMessage response = new HttpRequestMessage();
+
+            //response.Content.Headers.ContentType = new MediaTypeHeaderValue()
+            //return Ok(response);
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+
+            string localfilepath = @"D:\GitHUB\WebAPIMessageHandlers\WebAPIMessageHandlers\Content\Images\aspnet-web-api-poster.pdf";
+
+            byte[] bytes = File.ReadAllBytes(localfilepath);
+
+            response.Content = new ByteArrayContent(bytes);
+
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment"); //For Download, if not not needed , this line and below can be commented so that browsers PDF viewer displays the PDF
+
+            response.Content.Headers.ContentDisposition.FileName = "Web API Process Life Cycle.pdf";
+
+            return ResponseMessage(response);
+        }
+    }
+}
